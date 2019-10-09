d6595873e00017db17cfddb62afda1970e425bcc
Andrew Zhu <me@andrew-zhu.com>
2019-10-09 10:23:20 -0700

    add lambda IAM/SG

--- modules/avrae-analytics/outputs.tf
+++ modules/avrae-analytics/outputs.tf
@@ -1,3 +1,7 @@
 output "security_group_id" {
   value = aws_security_group.analytics_dms_access.id
 }
+
+output "lambda_security_group_id" {
+  value = aws_security_group.analytics_daily_lambda.id
+}
