69baeb553e6cd570ac5f748e517eebad1168d5e6
Andrew Zhu <me@andrew-zhu.com>
2020-05-22 13:53:29 -0700

    Update outputs.tf

--- modules/character-computation/outputs.tf
+++ modules/character-computation/outputs.tf
@@ -1,3 +1,3 @@
 output "api_endpoint" {
-  value = "https://${aws_api_gateway_rest_api.character_computation_api.id}.us-east-1.amazonaws.com/prod"
+  value = "https://${aws_api_gateway_rest_api.character_computation_api.id}.execute-api.us-east-1.amazonaws.com/prod"
 }
